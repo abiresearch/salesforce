171540850ab20b437574909b94445531a36b8a80
Peter Baehr <pete.baehr@gmail.com>
2018-07-20 11:37:29 -0400

    Cleanup

--- salesforce/connection.py
+++ salesforce/connection.py
@@ -12,20 +12,23 @@ import requests
 class JwtConnection:
     def __init__(self, consumer_id, username, signing_key_filename):
         jwt_header = {"alg": "RS256"}
-        encoded_jwt_header = urlsafe_b64encode(text_type(jwt_header).encode('UTF-8'))
+        encoded_jwt_header = urlsafe_b64encode(
+                text_type(jwt_header).encode('UTF-8'))
 
         claims_set = {'iss': consumer_id,
                       'sub': username,
                       'aud': 'https://login.salesforce.com',
                       'exp': str(int(time.time()))}
 
-        encoded_claims_set = urlsafe_b64encode(text_type(claims_set).encode('UTF-8'))
+        encoded_claims_set = urlsafe_b64encode(
+                text_type(claims_set).encode('UTF-8'))
 
         jwt_bearer_token = encoded_jwt_header + b'.' + encoded_claims_set
 
         key = RSA.importKey(open(signing_key_filename, 'rb').read())
         signer = PKCS1_v1_5.new(key)
-        signature = urlsafe_b64encode(signer.sign(SHA256.new(jwt_bearer_token)))
+        signature = urlsafe_b64encode(
+                signer.sign(SHA256.new(jwt_bearer_token)))
 
         signed_jwt_bearer_token = jwt_bearer_token + b'.' + signature
 
@@ -46,10 +49,12 @@ class JwtConnection:
         return requests.get(url, headers=self._get_headers())
 
     def post(self, url, payload):
-        return requests.post(url, data=json.dumps(payload), headers=self._get_headers())
+        return requests.post(url, data=json.dumps(payload),
+                             headers=self._get_headers())
 
     def patch(self, url, payload):
-        return requests.patch(url, data=json.dumps(payload), headers=self._get_headers())
+        return requests.patch(url, data=json.dumps(payload),
+                              headers=self._get_headers())
 
     def delete(self, url):
         return requests.delete(url, headers=self._get_headers())
